7144437c328ab7379d5345028d668847cc27cb8e
stepanosokin <stepanosokin@gmail.com>
2025-08-29 17:21:35 +0300

    доделал водные объекты

--- hse_pkp.py
+++ hse_pkp.py
@@ -1,5 +1,6 @@
 # import shapely
 import geopandas as gpd
+import pandas as pd
 from pyproj import Geod, crs, Transformer
 import shapely
 import re
@@ -8,13 +9,16 @@ import re
 def calculate_geod_buffers(
     i_gdf: gpd.GeoDataFrame,
     buffer_crs: str,
-    buffer_distance: float
+    buffer_dist_source: str,
+    buffer_distance
 ):
     """_summary_
 
     Args:
         i_gdf (gpd.GeoDataFrame): входной набор данных
         buffer_crs (str): проекция для расчета буфера: 'utm' или 'laea'
+        buffer_dist_source (str): источник значения размера буфера: 'field' (столбец в датафрейме) или 'value' (фикс значение)
+        buffer_distance: в зависимости от значения buffer_dist_source - название столбца (str) или фикс значение (float)
 
     Raises:
         ValueError: Если Некорректно задан параметр buffer_crs
@@ -45,7 +49,12 @@ def calculate_geod_buffers(
         transformer1 = Transformer.from_crs(4326, buf_crs, always_xy=True)
         transformer2 = Transformer.from_crs(buf_crs, 4326, always_xy=True)
         # вычисление буфера в текущей проекции UTM
-        buffer = shapely.buffer(shapely.transform(row['geometry'], transformer1.transform, interleaved=False), buffer_distance)
+        if buffer_dist_source == 'value':
+            buffer = shapely.buffer(shapely.transform(row['geometry'], transformer1.transform, interleaved=False), float(buffer_distance))
+        elif buffer_dist_source == 'field':
+            buffer = shapely.buffer(shapely.transform(row['geometry'], transformer1.transform, interleaved=False), float(row[buffer_distance]))
+        else:
+            raise ValueError("Неверно задан параметр buffer_dist_source")
         # Пересчет буфера обратно в WGS-1984
         buffer = shapely.transform(buffer, transformer2.transform, interleaved=False)
         # Добавить результат в список
@@ -53,18 +62,21 @@ def calculate_geod_buffers(
     return buffer_geom
 
 
-def prepare_water_line_limitations(
+def prepare_water_limitations(
     source_water_line='data/water_line.shp',
     source_water_pol='data/water_pol.shp',
+    result_gpkg='result/water_limitations.gpkg',
     buffer_distance = 5,
     buffer_crs = 'utm'
 ):
-    """Функция рассчитывает ограничения для проекта ПКП для линейных водных объектов по методике Жанны. 
+    """Функция рассчитывает ограничения для проекта ПКП для линейных и площадных водных объектов по методике Жанны. 
     Документация дорабатывается.
 
     Args:
-        source (str, optional): Исходные данные. Defaults to 'data/water_line.shp'.
-        buffer_distance (int, optional): расстояние буфера в м. Будет рассчитан в UTM с осевым меридианом в центроиде объекта или в LAEA с центром в центроиде объекта. Defaults to 5.
+        source_water_line (str, optional): Линейные водные объекты, исходные данные. Defaults to 'data/water_line.shp'.
+        source_water_pol (str, optional): Площадные водные объекты, исходные данные. Defaults to 'data/water_pol.shp'.
+        result_gpkg (str, optional): относительный путь к Geopackage для сохранения результатов.
+        buffer_distance (int, optional): расстояние первого буфера от линейных объектов в м. Будет рассчитан в UTM с осевым меридианом в центроиде объекта или в LAEA с центром в центроиде объекта. Defaults to 5.
         buffer_crs (str, optional): проекция для расчета буферов. 'utm' или 'laea'. Defaults to 'utm'.
     """
     try:
@@ -85,30 +97,35 @@ def prepare_water_line_limitations(
     # рассчитать геодезические длины
     geod = Geod(ellps='WGS84')
     gdf_l['leng'] = [geod.geometry_length(row['geometry']) for i, row in gdf_l.iterrows()]    
-    # gdf.to_file('result/water_line.gpkg', layer='water_line_dissolved')
+    # gdf.to_file(result_gpkg, layer='water_line_dissolved')
 
     # разбить мульти полилинии на отдельные
     gdf_l = gdf_l.explode()
     
     # рассчитать геодезические длины после разбивки на отдельные линии
     gdf_l['leng'] = [geod.geometry_length(row['geometry']) for i, row in gdf_l.iterrows()]    
-    # gdf.to_file('result/water_line.gpkg', layer='water_line_dissolved_exploded')
+    # gdf.to_file(result_gpkg, layer='water_line_dissolved_exploded')
     
     # отфильтровать ручьи и каналы короче 10 км
     gdf_l = gdf_l.query('not (fclass in("stream", "canal") and leng < 10000)')
-    # gdf.to_file('result/water_line.gpkg', layer='water_line_dissolved_exploded_filtered')
+    # gdf_l.to_file(result_gpkg, layer='water_line_dissolved_exploded_filtered')
     
     # Расчет буферов вокруг линейных водных объектов
-    buffer_geom = calculate_geod_buffers(gdf_l, buffer_crs, buffer_distance)
+    buffer_geom = calculate_geod_buffers(gdf_l, buffer_crs, 'value', buffer_distance)
     
     # gdf_l['buffer_geometry'] = buffer_geom
-    gdf_l = gdf_l.set_geometry(buffer_geom)
-    my_columns = gdf_l.columns.tolist()
+    gdf_l_buffered = gdf_l.set_geometry(buffer_geom)
+    my_columns = gdf_l_buffered.columns.tolist()
     # my_columns.remove('buffer_geometry')
-    gdf_l = gdf_l[my_columns]
-    
-    gdf_l['buf'] = [50 if x <= 10 else 100 if 10 < x <= 50 else 200 for x in gdf_l['leng']]
-    # gdf_l.to_file('result/water_line.gpkg', layer='water_lines_buf')
+    gdf_l_buffered = gdf_l_buffered[my_columns]
+    # расчет значений размеров буферов
+    gdf_l_buffered['buf'] = [50 if x <= 10000 else 100 if 10000 < x <= 50000 else 200 for x in gdf_l_buffered['leng']]
+    gdf_l['buf'] = [50 if x <= 10 else 100 if 10000 < x <= 50000 else 200 for x in gdf_l['leng']]
+    # gdf_l.to_file(result_gpkg, layer='water_lines_buf')
+    # вычислить геодезические буферы по полю buf  
+    buffer_geom = calculate_geod_buffers(gdf_l_buffered, buffer_crs, 'field', 'buf')
+    wp2 = gdf_l_buffered.set_geometry(buffer_geom)
+    # wp2.to_file(result_gpkg, layer='wp2')
     
     # ---------------------------------------
     # обработка полигональных водных объектов
@@ -120,6 +137,9 @@ def prepare_water_line_limitations(
     
     # Вычисление "длин" площадных объектов
     # Сделать пространственное присоединение обработанных линейных объектов по пересечению
+    gdf_l = gdf_l.clip(gdf_p)
+    gdf_l['leng'] = [geod.geometry_length(row['geometry']) for i, row in gdf_l.iterrows()]
+    # gdf_l.to_file(result_gpkg, layer='water_lines_clipped')
     gdf_p = gdf_p.sjoin(gdf_l, how='left')
     # Отсортировать результат по длине присоединенного объекта по убыванию
     gdf_p = gdf_p.sort_values(by='leng', ascending=False)
@@ -137,14 +157,32 @@ def prepare_water_line_limitations(
     
     # Там где не присоединилась длина линейного объекта и площадь больше 0.5, вставить buf=50
     gdf_p['buf'] = [50 if all([x >= 0.5, y]) else z for x, y, z in zip(gdf_p['area_km'], gdf_p['leng'].isna(), gdf_p['buf'])]
-    gdf_p.to_file('result/water_line.gpkg', layer='water_pol_leng')
+    # gdf_p.to_file(result_gpkg, layer='water_poly_leng')
+    # убрать объекты с пустыми значениями buf
+    gdf_p = gdf_p[gdf_p['buf'].notnull()]
+    # вычислить геодезические буферы по полю buf  
+    buffer_geom = calculate_geod_buffers(gdf_p, buffer_crs, 'field', 'buf')
+    wp1 = gdf_p.set_geometry(buffer_geom)
+    # wp1.to_file(result_gpkg, layer='wp1')
     
+    # Объединение двух итоговых слоев
+    wp1_wp2 = pd.concat([wp1, wp2])
+    # wp1_wp2.to_file('result/water_line.gpkg', layer='wp1_wp2')
+    water_prot_zone = wp1_wp2.union_all()
+    water_prot_zone = gpd.GeoDataFrame(gpd.GeoSeries(water_prot_zone))
+    water_prot_zone = water_prot_zone.rename(columns={0:'geometry'}).set_geometry('geometry')
+    water_prot_zone = water_prot_zone.set_crs('epsg:4326')
+    water_prot_zone = water_prot_zone.explode()
+    
+    water_prot_zone.to_file(result_gpkg, layer='water_prot_zone')
 
 
 if __name__ == '__main__':
-    prepare_water_line_limitations(
+    prepare_water_limitations(
         source_water_line='data/Lipetsk_water_lines_3857/Lipetsk_water_lines_3857.shp',
         source_water_pol='data/Lipetsk_water_poly_3857/Lipetsk_water_poly_3857.shp',
-        buffer_crs='utm'
+        result_gpkg='result/water_limitations.gpkg',
+        buffer_distance = 5,
+        buffer_crs = 'utm'
         )
     
